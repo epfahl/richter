b433d93d5ce8a44b28057d0b25535e27cee0ab1e
Eric Pfahl <eric.pfahl@gmail.com>
2022-06-30 09:44:58 -0400

    a simple MFA scheduler, plugged in as supervised application; stub call for testing

--- lib/richter/application.ex
+++ lib/richter/application.ex
@@ -1,19 +1,13 @@
 defmodule Richter.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
-  @moduledoc false
-
   use Application
 
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: Richter.Worker.start_link(arg)
-      # {Richter.Worker, arg}
+      {Richter.Scheduler,
+       [mfa: [Richter.Test, :test, ["Hi from scheduler!"]], period: :timer.seconds(3)]}
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Richter.Supervisor]
     Supervisor.start_link(children, opts)
   end
---
+++ lib/richter/scheduler.ex
@@ -0,0 +1,36 @@
+defmodule Richter.Scheduler do
+  @moduledoc """
+  A simple GenServer-based scheduler that accepts an MFA argument for a function
+  to call quasi-periodically. In this implementation, the schduler is blocked
+  until the function execution completes. This will introduced drift into the
+  timing. If strict periodicity is required, the function could be called
+  asynchronously.
+
+  A more robust, but still relatively simple, solution would use something like
+  the `Quantum` library to achieve cron-like functionality.
+  """
+  use GenServer
+
+  def start_link([mfa: [_module, _function, _args], period: _period] = state) do
+    GenServer.start_link(__MODULE__, state, name: __MODULE__)
+  end
+
+  def init(state) do
+    execute(state)
+    {:ok, state}
+  end
+
+  def handle_info(:work, state) do
+    execute(state)
+    {:noreply, state}
+  end
+
+  def execute(mfa: [module, function, args], period: period) do
+    apply(module, function, args)
+    Process.send_after(self(), :work, period)
+  end
+end
+
+defmodule Richter.Test do
+  def test(message), do: "Message: #{message}" |> IO.inspect()
+end

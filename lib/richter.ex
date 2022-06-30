6c1a618055921b61e9a639c0721ab0517c6770b9
Eric Pfahl <eric.pfahl@gmail.com>
2022-06-30 08:59:14 -0400

    empty supervised Elixir application created with * creating README.md * creating .formatter.exs * creating .gitignore * creating mix.exs * creating lib * creating lib/richter.ex * creating lib/richter/application.ex * creating test * creating test/test_helper.exs * creating test/richter_test.exs
    Your Mix project was created successfully.
    You can use "mix" to compile it, test it, and more:
    
        cd richter
        mix test
    
    Run "mix help" for more commands.

---
+++ lib/richter.ex
@@ -0,0 +1,18 @@
+defmodule Richter do
+  @moduledoc """
+  Documentation for `Richter`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Richter.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/richter_test.exs
@@ -0,0 +1,8 @@
+defmodule RichterTest do
+  use ExUnit.Case
+  doctest Richter
+
+  test "greets the world" do
+    assert Richter.hello() == :world
+  end
+end

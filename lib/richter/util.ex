0026dac44088b126c31721971b8e77f96b41328f
Eric Pfahl <eric.pfahl@gmail.com>
2022-07-03 19:29:12 -0400

    add a couple doctests for funzies

--- lib/richter/util.ex
+++ lib/richter/util.ex
@@ -21,4 +21,34 @@ defmodule Richter.Util do
       {:error, _error} -> {:error, "something bad happened"}
     end
   end
+
+  @doc """
+  Compute the difference between two `DateTime` values in hours.
+
+  ## Examples
+
+    iex> dt1 = ~N[2022-07-03 01:00:00] |> DateTime.from_naive!("Etc/UTC")
+    iex> dt2 = ~N[2022-07-03 02:00:00] |> DateTime.from_naive!("Etc/UTC")
+    iex> Richter.Util.datetime_diff_hours(dt2, dt1)
+    1.0
+  """
+  def datetime_diff_hours(dt1, dt2) do
+    DateTime.diff(dt1, dt2) / 3600.0
+  end
+
+  @doc """
+  Delete `keys` from a `map`.
+
+  ## Examples
+
+    iex> map = %{a: 1, b: 2, c: 3, d: 4}
+    iex> keys = [:b, :d]
+    iex> Richter.Util.delete_keys(map, keys)
+    %{a: 1, c: 3}
+  """
+  def delete_keys(map, keys) when is_map(map) and is_list(keys) do
+    map
+    |> Enum.filter(fn {k, _v} -> not Enum.member?(keys, k) end)
+    |> Enum.into(%{})
+  end
 end

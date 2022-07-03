84c2739509bae0307da666b4e9fe157bfce80889
Eric Pfahl <eric.pfahl@gmail.com>
2022-07-02 21:50:07 -0400

    add basic changeset func

--- lib/richter/schema/user_event.ex
+++ lib/richter/schema/user_event.ex
@@ -1,10 +1,17 @@
 defmodule Richter.Schema.UserEvent do
   use Ecto.Schema
+  import Ecto.Changeset
 
   schema "user_event" do
     belongs_to(:user, Richter.Schema.User, type: Ecto.UUID)
     belongs_to(:event, Richter.Schema.Event, type: :string)
 
     timestamps()
+  end
+
+  def changeset(user_event, params \\ %{}) do
+    user_event
+    |> cast(params, [:user_id, :event_id])
+    |> validate_required([:user_id, :event_id])
   end
 end

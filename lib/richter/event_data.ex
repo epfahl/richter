af8a098bed140841e45131942826de24176e1397
Eric Pfahl <eric.pfahl@gmail.com>
2022-07-03 09:48:47 -0400

    update docs

--- lib/richter/event_data.ex
+++ lib/richter/event_data.ex
@@ -9,14 +9,26 @@ defmodule Richter.EventData do
 
   @doc """
   Get events from the last hour and insert new events into the database.
+
+  Notes
+  -----
+  * This does not check for new events relative to what's already stored. It
+    just inserts with impunity, counting on conflict handling to prevent dupes.
+    It might make sense to check for new events (ID set difference) before
+    insertion, but it's not clear if this would improve performance.
   """
   def get_and_insert_last_1hour_events(), do: get_and_insert_events(:hour)
 
   @doc """
   Get events from the last 30 days and insert new events into the database.
+
+  Notes
+  -----
+  * See note for `get_and_insert_last_1hour_events`.
   """
   def get_and_insert_last_30days_events(), do: get_and_insert_events(:month)
 
+  # Abstraction to handle event inserts.
   defp get_and_insert_events(interval) do
     with {:ok, %{"features" => events}} <- fetch_fun(interval).() do
       Query.insert_events(events)

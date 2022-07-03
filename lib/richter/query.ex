bf8155923815e15b8db3c02e5c19fe5f8d6c01f7
Eric Pfahl <eric.pfahl@gmail.com>
2022-07-03 08:36:03 -0400

    expanded docstrings for insert functions

--- lib/richter/query.ex
+++ lib/richter/query.ex
@@ -1,40 +1,89 @@
 defmodule Richter.Query do
   alias Richter.Repo
-  alias Richter.Schema.{User, Event, UserEvent}
+  alias Richter.Schema.{User, UserEvent}
   alias Richter.Transform, as: T
 
   import Ecto.Query
   import Geo.PostGIS
 
   @doc """
-  Given a normal map representing user data (e.g., from a subscription),
+  Given a normal map representing a user subscription payload,
   prepare and validate the data through a changeset. Insert the
   changeset if valid and return the User struct; otherwise, return
   a map of errors in the changeset.
+
+  Example `user` map:
+  %{
+    endpoint: "http://myurl.me",
+    filters: [
+      %{type: "magnitude", minimum: 1.0}
+    ]
+  }
   """
-  def insert_user(user) do
-    insert_one(User, user)
+  def insert_user(user_sub) do
+    insert_one(User, user_sub)
   end
 
   @doc """
-  Given a list of USGC earthquake events ("features") as normal maps,
-  prepare and validate the maps, and then perform a bulk insert.
+  Given a list of USGC earthquake features (earthquake events) as normal maps,
+  prepare and validate the maps, and then perform a bulk insert. These features
+  are first transformed into proper `Event` payloads before insertion.
 
   Note: `on_conflict: :nothing` is there so that we can attempt to insert
   the same event (same "id") multiple times without raising a constraint
   error. Many contiguous queries of hourly event data will return common
   events.
 
+  Example feature map:
+  %{
+    "geometry" => %{
+      "coordinates" => [-66.9626666666667, 17.9648333333333, 8.36],
+      "type" => "Point"
+    },
+    "id" => "pr71357203",
+    "properties" => %{
+      "alert" => nil,
+      "cdi" => nil,
+      "code" => "71357203",
+      "detail" => "https://earthquake.usgs.gov/earthquakes/feed/v1.0/detail/pr71357203.geojson",
+      "dmin" => nil,
+      "felt" => nil,
+      "gap" => 192,
+      "ids" => ",pr71357203,",
+      "mag" => 2.01,
+      "magType" => "md",
+      "mmi" => nil,
+      "net" => "pr",
+      "nst" => 5,
+      "place" => "5 km WSW of Fuig, Puerto Rico",
+      "rms" => 0.08,
+      "sig" => 62,
+      "sources" => ",pr,",
+      "status" => "reviewed",
+      "time" => 1656793067010,
+      "title" => "M 2.0 - 5 km WSW of Fuig, Puerto Rico",
+      "tsunami" => 0,
+      "type" => "earthquake",
+      "types" => ",origin,phase-data,",
+      "tz" => nil,
+      "updated" => 1656794149300,
+      "url" => "https://earthquake.usgs.gov/earthquakes/eventpage/pr71357203"
+    },
+    "type" => "Feature"
+  }
+
   TODO
   ----
-  * Consolidate changeset processing and insert so that all the insert
+  * Consolidate changeset processing and insert logic so that all the insert
     functions can share similar abstractions.
   """
-  def insert_events(events) do
+  def insert_events(features) do
     changesets =
-      events
+      features
       |> T.prepare_event_list()
 
+    # `Repo.transaction` was needed here because `Repo.insert_all` (bulk
+    # insert) doesn't play nice with DB-autogenertated timestamps.
     Repo.transaction(fn ->
       changesets
       |> Enum.each(&Repo.insert!(&1, on_conflict: :nothing))
@@ -42,6 +91,13 @@ defmodule Richter.Query do
   end
 
   @doc """
+  Insert a user-event join entry representing a sent notification.
+
+  Example user-event payload:
+  %{
+    user_id: "d069ac4c-839f-4cb9-8f66-a58bd80a8350",
+    event_id: "pr71357203"
+  }
   """
   def insert_user_event(user_event) do
     insert_one(UserEvent, user_event)
@@ -58,6 +114,8 @@ defmodule Richter.Query do
     end
   end
 
+  # Extract changeset errors into a list of maps of the form:
+  # [%{key: reason}, ...]
   defp extract_changeset_errors(changeset) do
     for {k, v} <-
           changeset.errors,

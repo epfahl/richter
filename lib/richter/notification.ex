34b64abc597bb40da267892674febd0975b05998
Eric Pfahl <eric.pfahl@gmail.com>
2022-07-03 14:57:08 -0400

    add (unclean) async code to handle notification batch per user

--- lib/richter/notification.ex
+++ lib/richter/notification.ex
@@ -14,46 +14,48 @@ defmodule Richter.Notification do
     Checking for notifications at #{DateTime.utc_now()}.
     """)
 
-    users_with_events =
-      Q.get_all_users()
-      |> Enum.reduce([], fn u, acc ->
-        new_events = Q.get_new_user_events_details(u.id, u.filters)
+    Q.get_all_users()
+    |> Enum.each(fn u ->
+      user_events = Q.get_new_user_events(u.id, u.filters)
 
-        if new_events != [] do
-          user_events = %{
-            user_id: u.id,
-            endpoint: u.endpoint,
-            events: new_events
-          }
-
-          [user_events | acc]
-        else
-          acc
-        end
-      end)
-
-    users_with_events |> IO.inspect(label: "users with events")
-
-    # users = Q.get_all_users()
-    # # per user, check for new events
-    # # if list non-empty, queue notifications
-    # # put this on a separate schedule (same period)
+      if user_events != [] do
+        # FIXME: This concurrently sends a batch of user notifications, but
+        # this approach (no linking, no supervision) assumes we don't care about
+        # the result or the failure of the operation. It's also not sensitive to
+        # network resources or timeouts.
+        Task.start(fn ->
+          sent_all_user_notifications(u.id, u.endpoint, user_events)
+        end)
+      end
+    end)
   end
 
   @doc """
-  Post a single earthquake event to the webhook URL of a given user.
+  For a given `user_id` and `webhook_url` and list of `events`, send a
+  separate notification for each event. These notifications are sent
+  sequentially to avoid overwhelming the receiver. A sleep could be added as
+  simple (but hacky) rate limiter.
   """
-  def send_notification(user_id, webhook_url, event) do
-    with {:ok, _response_body} <- U.request(webhook_url, :post, json: event) do
+  def sent_all_user_notifications(user_id, webhook_url, events) do
+    events
+    |> Enum.each(&send_user_notification(user_id, webhook_url, &1))
+  end
+
+  @doc """
+  Post a single earthquake event to the webhook URL of a given user. On acknowledgement
+  of the request, an entry is addeed to the `UserEvent` join table.
+  """
+  def send_user_notification(user_id, webhook_url, %{id: event_id, details: details}) do
+    with {:ok, _response_body} <- U.request(webhook_url, :post, json: details) do
       IO.puts("""
       Notification successfully sent.
         User: #{user_id}
-        Event: #{event.id}
+        Event: #{event_id}
         Time: #{DateTime.utc_now()}
       """)
 
       # Insert user-event join on notification request acknoledgement
-      Q.insert_user_event(%{user_id: user_id, event_id: event.id})
+      Q.insert_user_event(%{user_id: user_id, event_id: event_id})
     end
   end
 end
